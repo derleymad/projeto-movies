2e0748d210f4153d2ebe83a40eb5c13264bfa8a3
derleymad <70535956+derleymad@users.noreply.github.com>
2022-07-20 12:45:12 -0300

    adding api

--- app/src/main/java/com/github/derleymad/youflix/model/Category.kt
+++
@@ -1,3 +0,0 @@
-package com.github.derleymad.youflix.model
-
-data class Category(val name: String, val movies:List<Movie>)
--- app/src/main/java/com/github/derleymad/youflix/model/Movie.kt
+++
@@ -1,5 +0,0 @@
-package com.github.derleymad.youflix.model
-
-import androidx.annotation.DrawableRes
-
-data class Movie (@DrawableRes val img: Int,val id:Int)
